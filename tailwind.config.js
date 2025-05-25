a8a751b999ab4894549a591c1998ab0a991ea550
Wafiul Achdi <achdiwafiul@gmail.com>
2025-05-25 20:20:30 +0700

    feat: add navigation bar for weather app

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,17 @@ export default {
         'weather-primary': '#00668A',
         'weather-secondary': '#004E71',
       },
-    },
-    fontFamily: {
-      Roboto: ['Roboto, sans-serif'],
-    },
-    container: {
-      padding: '2rem',
-      center: true,
-    },
-    screens: {
-      sm: '640px',
-      md: '768px',
+      fontFamily: {
+        Roboto: ['Roboto', 'sans-serif'],
+      },
+      container: {
+        padding: '2rem',
+        center: true,
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+      },
     },
   },
   plugins: [],
